4df6db661499d6b10e2500b488aba3a67b9e4044
iamkennethreyt <keba.tamis@swu.edu.ph>
2018-12-20 12:18:02 +0800

    Redux Login Action & Set Current User

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
 
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 //Register User
 export const registerUser = (userdata, history) => dispatch => {
@@ -13,4 +15,40 @@ export const registerUser = (userdata, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+//Login User
+export const loginUser = (userdata, history) => dispatch => {
+  axios
+    .post("/api/users/login", userdata)
+    .then(res => {
+      //save to local storage
+      const { token } = res.data;
+
+      //set token to localstorage
+      localStorage.setItem("jwtToken", token);
+
+      //set token to auth header
+      setAuthToken(token);
+
+      //decode token to get user data
+      const decoded = jwt_decode(token);
+
+      //set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
